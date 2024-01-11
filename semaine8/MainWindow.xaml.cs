3913cafe8ab7bfea6ef4d4a64eff4d86c2e6a543
francois.vandeputte <francois.vandeputte@student.vinci.be>
2024-01-11 14:33:43 +0100

    fin cours 8

--- semaine8/MainWindow.xaml.cs
+++ semaine8/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace semaine8
 {
@@ -23,6 +12,23 @@ namespace semaine8
         public MainWindow()
         {
             InitializeComponent();
+
+            PlaceData placesData = new PlaceData();
+            this.listBoxPhotos.DataContext = placesData.PlacesList;
+        }
+
+        private void listBoxPhotos_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            Place place = (Place)listBoxPhotos.SelectedItem;
+            BitmapSource photo = BitmapFrame.Create(new Uri(place.Path));
+            image1.Source = photo;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Place place = (Place)listBoxPhotos.SelectedItem;
+            place.Vote();
         }
     }
 }
---
+++ semaine8/PlaceData.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+
+namespace semaine8
+{
+    public class PlaceData
+    {
+        private IList<Place> placesList;
+
+        public PlaceData()
+        {
+            string pathProject = Environment.CurrentDirectory;
+            Place p1 = new (pathProject + "/photos/bruxelles.jpg", "Bruxelles");
+            Place p2 = new (pathProject + "/photos/paris.jpg", "Paris");
+            Place p3 = new (pathProject + "/photos/moscou.jpg", "Moscou");
+            Place p4 = new (pathProject + "/photos/amsterdam.jpg", "Amsterdam");
+            Place p5 = new (pathProject + "/photos/newyork.jpg", "New York");
+
+            placesList = new List<Place> { p1, p2, p3, p4, p5 };
+        }
+
+        public IList<Place> PlacesList
+        {
+            get { return placesList; }
+        }
+    }
+    public class Place
+    {
+        private string _description;
+        private string _pathImageFile;
+        private int _nbVotes;
+        private Uri _uri;
+        private BitmapFrame _image;
+
+        public Place(string path, string description)
+        {
+            _description = description;
+            _pathImageFile = path;
+            _nbVotes = 0;
+            _uri = new Uri(_pathImageFile);
+            _image = BitmapFrame.Create(_uri);
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public string Path
+        {
+            get { return _pathImageFile; }
+            set { _pathImageFile = value; }
+        }
+
+        public int NbVotes
+        {
+            get { return _nbVotes; }
+        }
+
+        public void Vote()
+        {
+            _nbVotes++;
+        }
+
+        public BitmapFrame Image { get { return _image; } }
+    }
+}
